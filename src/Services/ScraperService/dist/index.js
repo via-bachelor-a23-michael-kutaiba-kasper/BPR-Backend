b70b8fe798d3c36df7acee103a36427055252cb1
Michael Bui <michaelbui99@hotmail.com>
2023-09-25 20:22:49 +0200

    Reapply gitignore

--- src/Services/ScraperService/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const load_config_1 = require("./Configuration/load-config");
-const config = (0, load_config_1.loadConfig)();
-console.log(config);
-//# sourceMappingURL=index.js.map
