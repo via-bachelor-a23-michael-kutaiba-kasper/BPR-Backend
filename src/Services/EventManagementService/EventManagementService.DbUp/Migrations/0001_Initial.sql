8063e7f626c638f55a7916960c70830e6c01ce68
Michael Bui <michaelbui99@hotmail.com>
2023-09-27 15:21:38 +0200

    Setup postgres - Michael, Kutaiba, Kasper

---
+++ src/Services/EventManagementService/EventManagementService.DbUp/Migrations/0001_Initial.sql
@@ -0,0 +1,9 @@
+create table if not exists Event(
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    title varchar NOT NULL,
+    url varchar NOT NULL,
+    location varchar NOT NULL,
+    description varchar,
+                                
+    PRIMARY KEY (id)
+)
