ee9ae55bea0492b39d10fa972b439c0415c35ceb
Michael Bui <michaelbui99@hotmail.com>
2023-10-25 19:08:18 +0200

    scheduled jobs terraform

--- src/Infrastructure/Cloud/job_scheduler.tf
+++ src/Infrastructure/Cloud/job_scheduler.tf
@@ -1,78 +1,95 @@
+# APIs to enable
 resource "google_project_service" "cloudscheduler_api" {
   service            = "cloudscheduler.googleapis.com"
   disable_on_destroy = false
   project            = var.gcp_project_id
 }
-
-resource "google_cloud_run_v2_job" "periodic_jobs" {
-  name     = "periodic-jobs"
-  location = var.gcp_region
-
-  template {
-    template {
-      containers {
-        image = "docker.io/michaelbui293886/vibeverse-httpjob"
-
-        env {
-          name  = "JOB_URL"
-          value = module.scraper_service.service_url
-        }
-
-        env {
-          name  = "JOB_METHOD"
-          value = "POST"
-        }
-
-        env {
-          name = "JOB_BODY"
-          value = jsonencode({
-            strategy = "faengslet"
-          })
-        }
-      }
-    }
-  }
-
-  lifecycle {
-    ignore_changes = [
-      launch_stage,
-    ]
-  }
+resource "google_project_service" "iam_api" {
+  service            = "iam.googleapis.com"
+  disable_on_destroy = false
 }
 
-resource "google_cloud_scheduler_job" "job" {
-  provider         = google
-  name             = "periodic-jobs-scheduler"
-  description      = "Periodically calls the periodic_jobs cloud run job"
-  schedule         = "*/15 * * * *"
-  attempt_deadline = "320s"
-  region           = var.gcp_region
-  project          = var.gcp_project_id
+# Jobs
+# resource "google_cloud_run_v2_job" "periodic_jobs" {
+#   name     = "periodic-jobs"
+#   location = var.gcp_region
 
-  retry_config {
-    retry_count = 3
-  }
-  http_target {
-    http_method = "POST"
-    uri         = "https://${google_cloud_run_v2_job.periodic_jobs.location}-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/${var.gcp_project_number}/jobs/${google_cloud_run_v2_job.periodic_jobs.name}:run"
-  }
+#   template {
+#     template {
+#       containers {
+#         image = "docker.io/michaelbui293886/vibeverse-httpjob"
 
-  depends_on = [resource.google_project_service.cloudscheduler_api, resource.google_cloud_run_v2_job.periodic_jobs]
-}
+#         env {
+#           name = "JOBS"
+#           value = jsonencode([
+#             {
+#               url    = "${module.scraper_service.service_url}/scrape"
+#               method = "POST"
+#               body   = { strategy = "faengslet" }
+#             },
+#             {
+#               url    = "${module.eventmanagement_service.service_url}/api/v1/events/allPublicEvents"
+#               method = "GET"
+#             }
+#           ])
+#         }
+#       }
+#     }
+#   }
 
-data "google_iam_policy" "no_auth_policy" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers"
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       launch_stage,
+#     ]
+#   }
+# }
 
-resource "google_cloud_run_v2_job_iam_policy" "periodic_jobs_iam_policy" {
-  name     = google_cloud_run_v2_job.periodic_jobs.name
-  project  = google_cloud_run_v2_job.periodic_jobs.project
-  location = google_cloud_run_v2_job.periodic_jobs.location
+# # Scheduler
+# resource "google_cloud_scheduler_job" "job" {
+#   provider         = google
+#   name             = "periodic-jobs-scheduler"
+#   description      = "Periodically calls the periodic_jobs cloud run job"
+#   schedule         = "*/15 * * * *"
+#   attempt_deadline = "320s"
+#   region           = var.gcp_region
+#   project          = var.gcp_project_id
 
-  policy_data = data.google_iam_policy.no_auth_policy.policy_data
-}
+#   retry_config {
+#     retry_count = 3
+#   }
+#   http_target {
+#     http_method = "POST"
+#     uri         = "https://${google_cloud_run_v2_job.periodic_jobs.location}-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/${var.gcp_project_number}/jobs/${google_cloud_run_v2_job.periodic_jobs.name}:run"
+
+#     oidc_token {
+#       service_account_email = google_service_account.job_invoker.email
+#     }
+#   }
+
+#   depends_on = [resource.google_project_service.cloudscheduler_api, resource.google_cloud_run_v2_job.periodic_jobs, google_cloud_run_v2_job_iam_binding.binding]
+# }
+
+
+# # IAM
+# resource "google_service_account" "job_invoker" {
+#   account_id   = "scheduler-sa"
+#   description  = "Cloud Scheduler service account; used to trigger scheduled Cloud Run jobs."
+#   display_name = "scheduler-sa"
+#   project      = var.gcp_project_id
+#   provider     = google
+# }
+
+# resource "google_project_iam_binding" "job_invoker_binding" {
+#   project = var.gcp_project_id
+#   role    = "roles/iam.serviceAccountTokenCreator"
+#   members = ["serviceAccount:${google_service_account.job_invoker.email}"]
+# }
+
+# resource "google_cloud_run_v2_job_iam_binding" "binding" {
+#   project    = var.gcp_project_id
+#   location   = var.gcp_region
+#   name       = google_cloud_run_v2_job.periodic_jobs.name
+#   role       = "roles/viewer"
+#   members    = ["serviceAccount:${google_service_account.job_invoker.email}"]
+#   depends_on = [google_cloud_run_v2_job.periodic_jobs]
+# }
