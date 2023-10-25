035c45a3ec8b8613526a4b9e36ae2c4b56aba82a
Kutaiba Nezar Kashmar <71587889+Kutaiba-Nezar-Kashmar@users.noreply.github.com>
2023-10-25 13:23:35 +0200

    GeoLocation repo finished

--- src/Services/EventManagementService/EventManagementService.Application/ScraperEvents/Repository/IGeoCoding.cs
+++ src/Services/EventManagementService/EventManagementService.Application/ScraperEvents/Repository/IGeoCoding.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using EventManagementService.Domain.Models.Google;
+using EventManagementService.Infrastructure.Exceptions;
 using Microsoft.Extensions.Logging;
 
 namespace EventManagementService.Application.ScraperEvents.Repository;
@@ -11,14 +13,53 @@ public interface IGeoCoding
 public class GeoCoding : IGeoCoding
 {
     private readonly ILogger<GeoCoding> _logger;
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey;
 
-    public GeoCoding(ILogger<GeoCoding> logger)
+    public GeoCoding(ILogger<GeoCoding> logger, IHttpClientFactory httpClientFactory)
     {
         _logger = logger;
+        _httpClient = httpClientFactory.CreateClient("HTTP_CLIENT");
+        ;
+        _apiKey = Environment.GetEnvironmentVariable("GOOGLE_API_KEY") ?? throw new InvalidOperationException();
     }
 
-    public Task<GeoLocation> FetchGeoLocationForAddress(string address)
+    public async Task<GeoLocation> FetchGeoLocationForAddress(string address)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation("Fetching the GeoLocation for address{Address}", address);
+        var uri = $"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={_apiKey}";
+
+        var result = await _httpClient.GetAsync(uri);
+
+        ValidateHttpResponse(result);
+        var responseString = await result.Content.ReadAsStringAsync();
+        ValidateResponseContent(responseString);
+
+        var response = JsonSerializer.Deserialize<GeoLocation>(responseString, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        return response!;
+    }
+
+    private static void ValidateHttpResponse(
+        HttpResponseMessage responseMessage)
+    {
+        if (!responseMessage.IsSuccessStatusCode)
+        {
+            throw new HttpException
+            (
+                $"Unsuccessful response, code: {responseMessage}"
+            );
+        }
+    }
+
+    private static void ValidateResponseContent(string content)
+    {
+        if (string.IsNullOrEmpty(content) || string.IsNullOrWhiteSpace(content))
+        {
+            throw new HttpResponseException("Response is either null or empty");
+        }
     }
 }
---
+++ src/Services/EventManagementService/EventManagementService.Infrastructure/Exceptions/HttpException.cs
@@ -0,0 +1,12 @@
+namespace EventManagementService.Infrastructure.Exceptions;
+
+public class HttpException : Exception
+{
+    public HttpException(string msg) : base(msg)
+    {
+    }
+
+    public HttpException() : base()
+    {
+    }
+}
