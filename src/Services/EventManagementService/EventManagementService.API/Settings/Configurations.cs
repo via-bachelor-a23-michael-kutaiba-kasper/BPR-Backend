8d7c504ca2fb54e07acdebf948ca976af1d30c02
Michael Bui <michaelbui99@hotmail.com>
2023-10-25 11:12:41 +0200

    Get correct section from appsettings

--- src/Services/EventManagementService/EventManagementService.API/Settings/Configurations.cs
+++ src/Services/EventManagementService/EventManagementService.API/Settings/Configurations.cs
@@ -6,7 +6,7 @@ internal static class Configurations
 {
     internal static IServiceCollection AddSettingsConfigurations(this IServiceCollection services, ConfigurationManager configuration)
     {
-        services.Configure<ConnectionStrings>(configuration.GetSection("Postgres"));
+        services.Configure<ConnectionStrings>(configuration.GetSection("ConnectionStrings"));
         services.Configure<PubSub>(configuration.GetSection("PubSub"));
         return services;
     }
