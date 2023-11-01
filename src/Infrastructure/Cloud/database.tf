fd9852b049fd0069fe3f75d0e20a2fec13346f1e
Michael Bui <michaelbui99@hotmail.com>
2023-11-01 16:19:00 +0100

    Test db

--- src/Infrastructure/Cloud/database.tf
+++ src/Infrastructure/Cloud/database.tf
@@ -10,6 +10,23 @@ resource "google_sql_database_instance" "main" {
   }
   deletion_protection = false
 }
+resource "google_sql_database_instance" "test" {
+  name             = "vibeverse-test-postgres"
+  database_version = "POSTGRES_15"
+  region           = "europe-west3"
+
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-f1-micro"
+  }
+  deletion_protection = false
+}
+
+output "db_test_url" {
+  value = google_sql_database_instance.test.public_ip_address
+}
+
 
 output "db_url" {
   value = google_sql_database_instance.main.public_ip_address
