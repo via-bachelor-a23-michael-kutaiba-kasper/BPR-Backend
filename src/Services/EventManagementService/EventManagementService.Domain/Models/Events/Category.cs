9e14a90ebe80895208e3768e53329e483e773efb
Michael Bui <michaelbui99@hotmail.com>
2023-11-15 12:44:28 +0100

    Update event domain poco

---
+++ src/Services/EventManagementService/EventManagementService.Domain/Models/Events/Category.cs
@@ -0,0 +1,7 @@
+namespace EventManagementService.Domain.Models.Events;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
--- src/Services/EventManagementService/EventManagementService.Domain/Models/Events/Event.cs
+++ src/Services/EventManagementService/EventManagementService.Domain/Models/Events/Event.cs
@@ -2,9 +2,22 @@ namespace EventManagementService.Domain.Models.Events;
 
 public class Event
 {
+    public int Id { get; set; }
     public string Title { get; set; }
-    public Location Location { get; set; }
-    public string Url{ get; set; }
+    public DateTimeOffset StartDate { get; set; }
+    public DateTimeOffset EndDate { get; set; }
+    public DateTimeOffset CreatedDate { get; set; }
+    public DateTimeOffset LastUpdateDate { get; set; }
+    public bool IsPrivate { get; set; }
+    public bool AdultsOnly { get; set; }
+    public bool IsPaid { get; set; }
+    public string HostId { get; set; }
+    public int MaxNumberOfAttendees { get; set; }
+    public string Url { get; set; }
     public string? Description { get; set; }
-    
+    public Location Location { get; set; }
+    public string EventCode { get; set; }
+    public Category Category { get; set; }
+    public IEnumerable<Keyword> Keywords { get; set; }
+    public IEnumerable<string> Images { get; set; }
 }
