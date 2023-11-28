4499850e022f5a3ae9c122117fbdbbfb0a03b895
Kutaiba Nezar Kashmar <71587889+Kutaiba-Nezar-Kashmar@users.noreply.github.com>
2023-11-28 14:27:25 +0100

    IntegrationTest

--- src/Services/EventManagementService/EventManagementService.Test/ProcessExternalEvents/IntegrationTests/ProcessExternalEventsTests.cs
+++ src/Services/EventManagementService/EventManagementService.Test/ProcessExternalEvents/IntegrationTests/ProcessExternalEventsTests.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using EventManagementService.Application.ProcessExternalEvents;
+using EventManagementService.Application.ProcessExternalEvents.Exceptions;
 using EventManagementService.Application.ProcessExternalEvents.Repository;
 using EventManagementService.Domain.Models.Events;
 using EventManagementService.Domain.Models.Google;
@@ -101,5 +102,77 @@ public class ProcessExternalEventsTests
 
         // Assert
         Assert.DoesNotThrowAsync(() => act.Invoke());
+    }
+    
+     [Test]
+    public async Task ProcessExternalEvents_HandleException_ThrowCreateNewEventsException()
+    {
+        // Arrange
+        var externalEventsPath = Path.Combine(
+            AppDomain.CurrentDomain.BaseDirectory,
+            "ProcessExternalEvents",
+            "Fakes",
+            "ExternalEventsResponse.json"
+        );
+        
+        var eventsPath = Path.Combine(
+            AppDomain.CurrentDomain.BaseDirectory,
+            "ProcessExternalEvents",
+            "Fakes",
+            "ProcessedExternalEvents.json"
+        );
+        
+        var geoPath = Path.Combine(
+            AppDomain.CurrentDomain.BaseDirectory,
+            "ProcessExternalEvents",
+            "Fakes",
+            "GoogleGeoCodeResponse.json"
+        );
+
+        var eventsFile = await File.ReadAllTextAsync(eventsPath);
+        var events = JsonSerializer.Deserialize<List<Event>>(eventsFile, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        });
+        
+        var externalEventsFile = await File.ReadAllTextAsync(externalEventsPath);
+        var externalEvents = JsonSerializer.Deserialize<List<Event>>(externalEventsFile, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        });
+
+        var geoFile = await File.ReadAllTextAsync(geoPath);
+        var geoLocations = JsonSerializer.Deserialize<GoogleGeoLocation>(geoFile, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        });
+
+        var loggerMock = new Mock<ILogger<ProcessExternalEventsHandler>>();
+        var pubSubRepoMock = new Mock<IPubSubExternalEvents>();
+        var sqlRepoMock = new Mock<ISqlExternalEvents>();
+        var geoCodingRepoMock = new Mock<IGeoCoding>();
+
+        pubSubRepoMock.Setup(x => x.FetchEvents(new CancellationToken())).ReturnsAsync(externalEvents!);
+        sqlRepoMock.Setup(x => x.BulkUpsertEvents(It.IsAny<List<Event>>())).ThrowsAsync(new UpsertEventsException());
+        geoCodingRepoMock.Setup(x => x.FetchGeoLocationForAddress("Fussingvej 8, 8700 Horsens, Denmark"))!
+            .ReturnsAsync(geoLocations);
+
+        var handler = new ProcessExternalEventsHandler(
+            geoCodingRepoMock.Object,
+            pubSubRepoMock.Object,
+            sqlRepoMock.Object,
+            loggerMock.Object
+        );
+        // Act
+        var act = async () => await handler.Handle(
+            new ProcessExternalEventsRequest(),
+            new CancellationToken()
+        );
+
+        // Assert
+        Assert.ThrowsAsync<CreateNewEventsException>(() => act.Invoke());
     }
 }
