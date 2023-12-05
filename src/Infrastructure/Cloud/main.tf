57878f5d44dd4cf268eb79536ebfe9f40a49566f
Kutaiba Nezar Kashmar <71587889+Kutaiba-Nezar-Kashmar@users.noreply.github.com>
2023-12-05 14:09:03 +0100

    query efined and terraform updated

--- src/Infrastructure/Cloud/main.tf
+++ src/Infrastructure/Cloud/main.tf
@@ -43,6 +43,7 @@ module "api_gateway" {
     "QUERY_CATEGORIES_HOST"      = module.eventmanagement_service.service_url
     "QUERY_KEYWORDS_HOST"        = module.eventmanagement_service.service_url
     "QUERY_CREATEREVIEW_HOST"    = module.eventmanagement_service.service_url
+    "QUERY_REVIEWSBYUSER_HOST"    = module.eventmanagement_service.service_url
   }
   cloud_sql_instance = google_sql_database_instance.main.connection_name
 }
