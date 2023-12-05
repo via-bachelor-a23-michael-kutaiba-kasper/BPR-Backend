efa8e6ec3057381ad688b6d5f368b573caafb0d9
Kutaiba Nezar Kashmar <71587889+Kutaiba-Nezar-Kashmar@users.noreply.github.com>
2023-12-05 14:17:57 +0100

    delete unused dto

--- src/Services/EventManagementService/EventManagementService.API/Controllers/V1/ReviewControllers/Dtos/FetchReviewsByUSerResponseDto.cs
+++
@@ -1,7 +0,0 @@
-namespace EventManagementService.API.Controllers.V1.ReviewControllers.Dtos;
-
-public class FetchReviewsByUSerResponseDto
-{
-    public EventReviewDto[] Result { get; set; }
-    public StatusCode status { get; set; }
-}
