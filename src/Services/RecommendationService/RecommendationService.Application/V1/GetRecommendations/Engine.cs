24d7a367509c08db4e00e45b5079d6d3cdd76344
Michael Bui <michaelbui99@hotmail.com>
2023-12-05 09:15:14 +0100

    filter events where user attended

--- src/Services/RecommendationService/RecommendationService.Application/V1/GetRecommendations/Engine.cs
+++ src/Services/RecommendationService/RecommendationService.Application/V1/GetRecommendations/Engine.cs
@@ -5,7 +5,7 @@ using RecommendationService.Domain;
 namespace RecommendationService.Application.V1.GetRecommendations;
 
 // NOTE: Keep this pure for easier testing, no side effects (network calls etc.)
-public interface IEngine
+public interface IRecommendationsEngine
 {
     /// <summary>
     /// Process recommendations for future events based on completed events the user has attended in the past.
@@ -16,4 +16,19 @@ public interface IEngine
     /// <returns></returns>
     Recommendations Process(User user, IReadOnlyCollection<Event> completedEvents,
         IReadOnlyCollection<Event> futureEvents);
+}
+
+public class RecommendationsEngine : IRecommendationsEngine
+{
+    public Recommendations Process(User user, IReadOnlyCollection<Event> completedEvents,
+        IReadOnlyCollection<Event> futureEvents)
+    {
+        IDictionary<Category, int> categoryFrequencyMap = new Dictionary<Category, int>();
+        IDictionary<Keyword, int> keywordFrequencyMap = new Dictionary<Keyword, int>();
+
+        var relevantEvents = completedEvents
+            .Where(e => e.Attendees != null)
+            .Where(e => e.Attendees.Any(u => u.UserId == user.UserId));
+        throw new NotImplementedException();
+    }
 }
