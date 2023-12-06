b8cedd139a50948191b1d26a49cac064e7e82e15
Michael Bui <michaelbui99@hotmail.com>
2023-12-06 18:30:26 +0100

    logging

--- src/Services/RecommendationService/RecommendationService.Domain/Events/Review.cs
+++ src/Services/RecommendationService/RecommendationService.Domain/Events/Review.cs
@@ -2,9 +2,9 @@ namespace RecommendationService.Domain.Events;
 
 public class Review
 {
-    public int Id { get; set; }
-    public float Rate { get; set; }
+    public int Id { get; set; } = default;
+    public float Rate { get; set; } = default;
     public string ReviewerId { get; set; } = default!;
-    public int EventId { get; set; }
+    public int EventId { get; set; } = default;
     public DateTimeOffset ReviewDate { get; set; } = default!;
 }
