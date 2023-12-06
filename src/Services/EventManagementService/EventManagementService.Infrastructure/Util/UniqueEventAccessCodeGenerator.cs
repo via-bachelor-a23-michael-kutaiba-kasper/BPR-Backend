7d38d43bc8f13dbdffc5f517dcb8c2fb4aa706a8
Kutaiba Nezar Kashmar <71587889+Kutaiba-Nezar-Kashmar@users.noreply.github.com>
2023-12-06 11:01:28 +0100

    big fixes in external events

--- src/Services/EventManagementService/EventManagementService.Infrastructure/Util/UniqueEventAccessCodeGenerator.cs
+++ src/Services/EventManagementService/EventManagementService.Infrastructure/Util/UniqueEventAccessCodeGenerator.cs
@@ -23,4 +23,22 @@ public static class UniqueEventAccessCodeGenerator
             return stringBuilder.ToString();
         }
     }
+    public static string GenerateUniqueStringForExternal(string title, string description, string hostId)
+    {
+        var combinedInfo = $"{title}_{description ?? ""}_{hostId}";
+
+        using (var sha256 = SHA256.Create())
+        {
+            var hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(combinedInfo));
+
+            // Convert the hashed bytes to a string
+            var stringBuilder = new StringBuilder();
+            foreach (var t in hashBytes)
+            {
+                stringBuilder.Append(t.ToString("x2"));
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
 }
