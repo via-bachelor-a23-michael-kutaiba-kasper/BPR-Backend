e4089431685e49d618e2248c597b04f3ad29d351
Kutaiba Nezar Kashmar <71587889+Kutaiba-Nezar-Kashmar@users.noreply.github.com>
2023-12-07 09:14:36 +0100

    naming fixes for event review dto

--- src/Services/EventManagementService/EventManagementService.API/Controllers/V1/ReviewControllers/Dtos/EventReviewDto.cs
+++ src/Services/EventManagementService/EventManagementService.API/Controllers/V1/ReviewControllers/Dtos/EventReviewDto.cs
@@ -2,9 +2,9 @@ namespace EventManagementService.API.Controllers.V1.ReviewControllers.Dtos;
 
 public class EventReviewDto
 {
-    public int ReviewId { get; set; }
+    public int Id { get; set; }
     public float Rate { get; set; }
-    public string UserId { get; set; }
+    public string ReviewerId { get; set; }
     public DateTimeOffset ReviewDate { get; set; }
     public int EventId { get; set; }
 }
