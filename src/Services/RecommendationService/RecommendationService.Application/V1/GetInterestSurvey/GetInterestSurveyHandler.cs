1086963e18c6cc69cf2fdcde01c587d8e11fa710
Michael Bui <michaelbui99@hotmail.com>
2023-12-08 10:04:57 +0100

    get interest survey slice

---
+++ src/Services/RecommendationService/RecommendationService.Application/V1/GetInterestSurvey/GetInterestSurveyHandler.cs
@@ -0,0 +1,42 @@
+using MediatR;
+using Microsoft.Extensions.Logging;
+using RecommendationService.Application.V1.GetInterestSurvey.Exceptions;
+using RecommendationService.Application.V1.GetInterestSurvey.Repositories;
+using RecommendationService.Domain.Events;
+
+namespace RecommendationService.Application.V1.GetInterestSurvey;
+
+public record GetInterestSurveyRequest(string userId) : IRequest<InterestSurvey?>;
+
+public class GetInterestSurveyHandler : IRequestHandler<GetInterestSurveyRequest, InterestSurvey?>
+{
+    private readonly ILogger<GetInterestSurveyHandler> _logger;
+    private readonly IInterestSurveyRepository _surveyRepository;
+    private readonly IUserRepository _userRepository;
+
+    public GetInterestSurveyHandler(ILogger<GetInterestSurveyHandler> logger,
+        IInterestSurveyRepository surveyRepository, IUserRepository userRepository)
+    {
+        _logger = logger;
+        _surveyRepository = surveyRepository;
+        _userRepository = userRepository;
+    }
+
+    public async Task<InterestSurvey?> Handle(GetInterestSurveyRequest request, CancellationToken cancellationToken)
+    {
+        var existingUser = await _userRepository.GetByIdAsync(request.userId);
+        if (existingUser is null)
+        {
+            throw new UserNotFoundException(request.userId);
+        }
+
+        _logger.LogInformation($"Retrieving interest survey for {request.userId}");
+        var interestSurvey = await _surveyRepository.GetInterestSurvey(request.userId);
+
+        _logger.LogInformation(interestSurvey is null
+            ? $"User {request.userId} has not yet filled out interest survey, returning null"
+            : $"Retrieved interest survey for user {request.userId} successfully");
+
+        return interestSurvey;
+    }
+}
