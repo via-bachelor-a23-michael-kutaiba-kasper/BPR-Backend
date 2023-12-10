d24c9e74078b28f0e35e340bea1506b6c98dd109
Michael Bui <michaelbui99@hotmail.com>
2023-12-10 15:54:50 +0100

    levels

--- src/Services/UserManagementService/UserManagementService.Domain/Models/Progress.cs
+++ src/Services/UserManagementService/UserManagementService.Domain/Models/Progress.cs
@@ -5,6 +5,7 @@ public class Progress
     public int Id { get; set; }
     public long TotalExp{ get; set; }
     public Level Level { get; set; } = new();
+    public IReadOnlyCollection<ExpProgressEntry> ExpProgressHistory = new List<ExpProgressEntry>();
     public MonthlyGoal MonthlyGoal { get; set; } = new();
     public IReadOnlyCollection<Unlockable> Unlockables = new List<Unlockable>();
 }
@@ -14,4 +15,10 @@ public class Level
     public long MaxExp { get; set; }
     public long MinExp { get; set; }
     public string Name { get; set; } = "";
+}
+
+public class ExpProgressEntry
+{
+    public DateTimeOffset Timestamp{ get; set; }
+    public long ExpGained { get; set; }
 }
