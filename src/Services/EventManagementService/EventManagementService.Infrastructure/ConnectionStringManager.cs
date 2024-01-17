834aaf53b60d395886a54065e4847e900f109e82
Michael Bui <michaelbui99@hotmail.com>
2024-01-17 11:17:59 +0100

    Update db connection strings

--- src/Services/EventManagementService/EventManagementService.Infrastructure/ConnectionStringManager.cs
+++ src/Services/EventManagementService/EventManagementService.Infrastructure/ConnectionStringManager.cs
@@ -38,8 +38,8 @@ public class ConnectionStringManager: IConnectionStringManager
         Console.WriteLine($"Deployment Environment: {_deploymentEnvironment}");
         
         // NOTE: This is only here due to it being a school project. Otherwise we would use a vault or pass by environment variable.
-        _connectionStrings.Add($"PRODUCTION", $"Server=34.159.177.93;Port=5432;Database=postgres;User Id=postgres;Password={password}");
-        _connectionStrings.Add($"CI", $"Server=34.107.115.110;Port=5432;Database=postgres;User Id=postgres;Password={password}");
+        _connectionStrings.Add($"PRODUCTION", $"Server=35.234.109.192 ;Port=5432;Database=postgres;User Id=postgres;Password={password}");
+        _connectionStrings.Add($"CI", $"Server=34.107.56.249;Port=5432;Database=postgres;User Id=postgres;Password={password}");
         _connectionStrings.Add($"LOCAL", $"Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=postgres");
         _connectionStrings.Add($"LOCAL_CONTAINER", $"Server=eventmanagement_postgres;Port=5432;Database=postgres;User Id=postgres;Password=postgres");
     }
